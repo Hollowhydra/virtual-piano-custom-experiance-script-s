acb8ecb4f59320675cc4dbdfcdf0db16ef4c3806
Hollowhydra <98896487+Hollowhydra@users.noreply.github.com>
2024-05-19 22:51:56 -0400

    Create disconnected textbox remover.js

---
+++ disconnected textbox remover.js
@@ -0,0 +1,3 @@
+ by Anzaz
+
+function removeIdlePop() { const idlePop = document.getElementById('idlePop'); if (idlePop) { idlePop.remove(); console.log('Idle pop-up removed'); } } setInterval(removeIdlePop, 1000);
